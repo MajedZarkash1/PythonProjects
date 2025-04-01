f415f3ce425d39d67a0b9bdb248977309c3cd276
Majed Zarkash <majedzarkash@gmail.com>
2025-04-01 12:50:55 +1100

    .

---
+++ Majd.py
@@ -0,0 +1,38 @@
+print("Hello!, world")
+input("Enter your name: ")
+
+x = 1 #int  
+y = 1 #int+ 
+z = y + x 
+print(z)
+
+name = "Majd" #string
+print(name)
+
+number = 1.5
+print(number)
+
+is_he_alive = False
+print(is_he_alive)
+
+
+def room1():
+    TV = 1
+    PC = "We have 2 PCs"
+
+    print(TV)
+    print(PC)
+    print(x)
+
+
+room1()
+
+
+
+
+
+
+
+
+
+
