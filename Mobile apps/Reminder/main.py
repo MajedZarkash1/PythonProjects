ed11b824541b95594aee2b56383b1758f121b0f2
Majed Zarkash <majedzarkash@gmail.com>
2025-04-21 11:50:16 +1000

    .

--- Mobile apps/Reminder/main.py
+++ Mobile apps/Reminder/main.py
@@ -1,11 +1,27 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
-class MyApp(App):
+class grid(GridLayout):
+    def __init__(self, **kwargs):
+        super(grid, self).__init__(**kwargs)
+
+        #set colums
+        self.cols = 2
+
+        #add widgets
+        self.add_widget(Label(text="name: "))
+        #add input
+        self.name = TextInput(multiline=False)
+        self.add_widget(self.name)
+
+class Myapp(App):
     def build(self):
-        return Label(text="Hello world") 
+        return grid()
     
 
 if __name__ == '__main__':
-    MyApp().run()
+    Myapp().run()
