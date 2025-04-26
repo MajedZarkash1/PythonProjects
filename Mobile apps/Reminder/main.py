d2837a213741f60801b1cfd67762139ac88f8dec
Majed Zarkash <majedzarkash@gmail.com>
2025-04-26 16:47:44 +1000

    Update main.py

--- Mobile apps/Reminder/main.py
+++ Mobile apps/Reminder/main.py
@@ -1,20 +1,54 @@
-import kivy #import kivy
-
-from kivy.app import App #Main
-from kivy.uix.label import Label #import Label
-from kivy.uix.button import Button #import Button
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 
-class MyApp(GridLayout): #the code should start here
+class Task(BoxLayout):
+    def __init__(self, text, **kwargs):
+        super().__init__(orientation='horizontal', size_hint_y=None, height=70, **kwargs)
+        self.label = Label(text=text, size_hint_x=0.7)
+        self.done_button = Button(text="Done", size_hint_x=0.15, on_press=self.mark_done)
+        self.delete_button = Button(text="Remove", size_hint_x=0.15, on_press=self.delete_task)
+        self.add_widget(self.label)
+        self.add_widget(self.done_button)
+        self.add_widget(self.delete_button)
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cols = 3
+    def mark_done(self, instance):
+        self.label.text = "[s]{}[/s]".format(self.label.text)  # strike-through
+        self.label.markup = True
 
-        self.add_widget(Label(text="Hello I am Label"))
-        self.add_widget(Button(text="Hello I am Button"))
-        
+    def delete_task(self, instance):
+        self.parent.remove_widget(self)
 
-    
-if __name__ == '__main__':
-    MyApp().run() #the code should end here
+class ReminderApp(App):
+    def build(self):
+        self.root = BoxLayout(orientation='vertical')
+
+        self.task_input = TextInput(hint_text="Enter a new task...", size_hint_y=None, height=50, multiline=False)
+        add_button = Button(text="Add Task", size_hint_y=None, height=50)
+        add_button.bind(on_press=self.add_task)
+
+        self.task_list_layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
+        self.task_list_layout.bind(minimum_height=self.task_list_layout.setter('height'))
+
+        scroll_view = ScrollView()
+        scroll_view.add_widget(self.task_list_layout)
+
+        self.root.add_widget(self.task_input)
+        self.root.add_widget(add_button)
+        self.root.add_widget(scroll_view)
+
+        return self.root
+
+    def add_task(self, instance):
+        task_text = self.task_input.text.strip()
+        if task_text:
+            task = Task(task_text)
+            self.task_list_layout.add_widget(task)
+            self.task_input.text = ""
+
+if __name__ == "__main__":
+    ReminderApp().run()
